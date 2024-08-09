2f2e1bc060cbd77bee08642736bdbdd2ff00819e
ArsalJafri <154714497+ArsalJafri@users.noreply.github.com>
2024-08-09 10:46:08 -0500

    First Commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,21 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import {userRouter} from "./routes/user.js"
-import { recipesRouter } from "./routes/recipes.js";
+import serverless from 'serverless-http';
+import { userRouter } from './routes/user.js';
+import { recipesRouter } from './routes/recipes.js';
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json()); 
-app.use(cors()); 
+// Routes
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
-app.use("/auth", userRouter)
-app.use("/recipes", recipesRouter)
+// Database connection
+mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connect(
-    "mongodb+srv://arsaljafri:oOw7nWZQMpKw9yJu@recipes.xunazaf.mongodb.net/recipes?retryWrites=true&w=majority",
-)
+// Serverless handler
+export const handler = serverless(app);
 
-app.listen(3001, () => console.log("SERVER STARTED!"))
